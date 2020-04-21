295ffea9fe64860421ffdff064f4b46c481e3314
Mozu Oishi <mozu.oishi@gmail.com>
2020-04-21 19:41:16 +0900

    Add a simple text area input that just echoes the same content in another div.

--- src/main.go
+++ src/main.go
@@ -8,4 +8,18 @@ var indexHTML = string(MustAsset("assets/index.html"))
 
 func main() {
 	js.Global.Get("document").Call("write", indexHTML)
+	inputTextArea().Set("oninput", onInput)
+}
+
+func inputTextArea() *js.Object {
+	return js.Global.Get("document").Call("querySelector", "#input")
+}
+
+func outputDiv() *js.Object {
+	return js.Global.Get("document").Call("querySelector", "#output")
+}
+
+func onInput() {
+	content := inputTextArea().Get("value").String()
+	outputDiv().Set("innerText", content)
 }
